230170616463ebb1d80dcad9ec51bc8be652a847
nelsost3 <91235854+nelsost3@users.noreply.github.com>
2023-09-15 14:27:56 -0600

    Add files via upload

---
+++ nelsost3-frontend/src/App.js
@@ -0,0 +1,54 @@
+// Import dependencies
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+// Import Components, styles, media
+import Navigation from './components/Navigation';
+import './App.css';
+
+// Import Pages
+import HomePage from './pages/HomePage';
+import AddExercisePage from './pages/AddExercisePage';
+import EditExercisePage from './pages/EditExercisePage';
+
+// Define the function that renders the content in routes using State.
+function App() {
+
+  const [exercise, setExercise] = useState([]);
+
+  return (
+    <>
+      <Router>
+
+          <header>
+            <h1>(✿◠‿◠) っ The Better Body Collection of Exercisess (◠‿◠✿)</h1>
+            <p>This app uses MERN. This app also works out.</p>
+          </header>
+
+          <Navigation />
+
+          <main>
+            <Route path="/" exact>
+              <HomePage setExercise={setExercise} />
+            </Route>
+
+            <Route path="/add-exercise">
+              <AddExercisePage />
+            </Route>
+            
+            <Route path="/edit-exercise">
+              <EditExercisePage exercise={exercise} />
+            </Route>
+          </main>
+
+          <footer>
+            <p>&copy; 2022 Steven Nelson ʕ •ᴥ•ʔ</p>
+          </footer>
+
+      </Router>
+    </>
+  );
+}
+
+export default App;
